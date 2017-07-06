c6b378168a9141072620e304f465ba27e18879e9
Kanaga2 <priyakanaga112@gmail.com>
2017-07-06 08:54:24 +0530

    Create Change.java

---
+++ Change.java
@@ -0,0 +1,20 @@
+# Change
+import java.io.*;
+import java.util.*;
+public class Change
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number);
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("It is a even number");
+}
+else
+{
+System.out.println("It is a odd number");
+]
+}
+}
